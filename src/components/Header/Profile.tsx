7d58c44a1dd2a4b3b60eac85595ac068fb0e81b3
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-07-29 18:38:40 -0300

    Separting components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,17 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return(
+    <Flex
+    align='center'
+  >
+    <Box>
+      <Text>Mateus de Sousa</Text>
+      <Text color='gray.300' fontSize='small'
+      >mateuso12@hotmail.com.br</Text>
+    </Box>
+
+    <Avatar size='md' name='Mateus de Sousa' ml='2' src='https://github.com/mateuso12.png' />
+  </Flex>
+  );
+}
