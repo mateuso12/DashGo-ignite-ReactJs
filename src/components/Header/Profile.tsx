07abb8fdf65e4b841d0fb90d6d57199452f2b93f
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-07-29 19:17:15 -0300

    Making header components responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return(
     <Flex
     align='center'
   >
-    <Box>
+    { showProfileData && (<Box mr='4' textAlign='right'>
       <Text>Mateus de Sousa</Text>
       <Text color='gray.300' fontSize='small'
       >mateuso12@hotmail.com.br</Text>
-    </Box>
+    </Box>)}
 
     <Avatar size='md' name='Mateus de Sousa' ml='2' src='https://github.com/mateuso12.png' />
   </Flex>
